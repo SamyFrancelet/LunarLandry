65940e58f7b1c6ece6bacf08e25edeb2b44e928a
landry <landry.reynard@students.hevs.ch>
2020-06-05 15:46:40 +0200

    ADD : Les images qui vont bien !

--- src/ch/hevs/gdx2d/lunar/main/Particle.java
+++ src/ch/hevs/gdx2d/lunar/main/Particle.java
@@ -18,7 +18,7 @@ import com.badlogic.gdx.physics.box2d.Filter;
 public class Particle extends PhysicsBox {
 	protected final int maxAge;
 	// Resources MUST not be static
-	protected BitmapImage img = new BitmapImage("data/images/fireball.png");
+	protected BitmapImage img = new BitmapImage("data/images/fireball_5.png");
 	protected int age = 0;
 	private boolean init = false;
 
@@ -59,7 +59,7 @@ public class Particle extends PhysicsBox {
 		g.sbSetColor(.5f, 0.7f, 0.9f, 1.0f - age / (float) (maxAge + 5));
 
 		// Draw the particle
-		g.draw(img.getRegion(), pos.x - img.getImage().getWidth() / 2, pos.y - img.getImage().getHeight() / 2);
+		g.draw(img.getRegion(), pos.x - img.getImage().getWidth() / 2, pos.y - 15 - img.getImage().getHeight() / 2);
 //		g.draw(new Texture("data/images/SpaceShip_2.png"), pos.x, pos.y, 10, 10);
 
 		g.sbSetColor(col);
