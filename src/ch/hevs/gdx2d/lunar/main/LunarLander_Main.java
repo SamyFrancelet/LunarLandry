56e4e81c2947b650825c5ebea72ed2e167e5e893
landry <landry.reynard@students.hevs.ch>
2020-06-16 16:29:32 +0200

    ADD : Little stuff

--- src/ch/hevs/gdx2d/lunar/main/LunarLander_Main.java
+++ src/ch/hevs/gdx2d/lunar/main/LunarLander_Main.java
@@ -26,11 +26,13 @@ import ch.hevs.gdx2d.lunar.physics.PhysicsSimulator;
 import ch.hevs.gdx2d.lunar.physics.Simulatable;
 import ch.hevs.gdx2d.desktop.PortableApplication;
 
-public class LunarLander_Main extends PortableApplication{
-	
+public class LunarLander_Main extends PortableApplication {
+
 	PhysicsSimulator physics = new PhysicsSimulator(Constants.WIN_WIDTH, Constants.WIN_HEIGHT);
 	Spaceship ssLandry = new Spaceship(new Vector2(400, 400));
-	
+	MusicPlayer music;
+	SoundSample reactor;
+
 	// Particle related
 	World world = PhysicsWorld.getInstance();
 	static final Random rand = new Random();
@@ -41,20 +43,16 @@ public class LunarLander_Main extends PortableApplication{
 	// Shooting related
 	boolean mouseActive = false;
 	Vector2 positionClick;
-	
-	
+
 	public LunarLander_Main() {
 		super(Constants.WIN_WIDTH, Constants.WIN_HEIGHT);
 	}
-	
+
 	@Override
 	public void onInit() {
 		setTitle("LunarLandry (Team PLS)");
 		physics.addSimulatableObject(ssLandry);
-		SoundSample music = new SoundSample("data\\sons\\sound1.mp3");
-		//MusicPlayer music = new MusicPlayer("data\\sons\\sound1.mp3");
-		music.loop();
-
+		playMusic();
 	}
 
 	@Override
@@ -65,13 +63,11 @@ public class LunarLander_Main extends PortableApplication{
 		g.drawFPS();
 		g.drawSchoolLogo();
 		g.drawLine(0, Constants.GROUND_ALTITUDE, Constants.WIN_WIDTH, Constants.GROUND_ALTITUDE, Color.WHITE);
-		
-		
+
 		Array<Body> bodies = new Array<Body>();
 		world.getBodies(bodies);
 
 		Iterator<Body> it = bodies.iterator();
-
 
 		while (it.hasNext()) {
 			Body p = it.next();
@@ -96,22 +92,27 @@ public class LunarLander_Main extends PortableApplication{
 			}
 		}
 		PhysicsWorld.updatePhysics(Gdx.graphics.getDeltaTime());
-		
 
 		createBackGRound();
 
-
-		if (ssLandry.thrustUp || ssLandry.thrustLeft || ssLandry.thrustRight)
+		if (ssLandry.thrustUp || ssLandry.thrustLeft || ssLandry.thrustRight) {
 			createParticles();
-		
+		}
+		if (mouseActive)
+			ssLandry.shoot(g, ssLandry.position, positionClick);
+
 		ssLandry.draw(g);
 
-	
-	
 	}
-	
+
+	void playMusic() {
+		// music = new SoundSample("data\\sons\\sound1.mp3");
+		music = new MusicPlayer("data\\sons\\sound1.mp3");
+		music.loop();
+	}
+
 	void createParticles() {
-		for (int i = 0; i < CREATION_RATE; i++ ) {
+		for (int i = 0; i < CREATION_RATE; i++) {
 			Particle c = new Particle(ssLandry.position, 10, MAX_AGE + rand.nextInt(MAX_AGE / 2));
 
 			// Apply a vertical force with some random horizontal component
@@ -119,39 +120,42 @@ public class LunarLander_Main extends PortableApplication{
 			ssLandry.force.y = rand.nextFloat() * 0.00095f * (rand.nextBoolean() == true ? -1 : 1);
 			c.applyBodyLinearImpulse(ssLandry.force, ssLandry.position, true);
 		}
+
 	}
-	
+
 	void createBackGRound() {
 		for (int i = 0; i < CREATION_STAR_RATE; i++) {
 			Vector2 random = new Vector2(rand.nextFloat() * Constants.WIN_WIDTH,
-					(rand.nextFloat() * (Constants.WIN_HEIGHT- Constants.GROUND_ALTITUDE))+ Constants.GROUND_ALTITUDE);
-			BackGround b = new BackGround(random, 10, MAX_STAR_AGE + rand.nextInt(MAX_STAR_AGE ));
+					(rand.nextFloat() * (Constants.WIN_HEIGHT - Constants.GROUND_ALTITUDE))
+							+ Constants.GROUND_ALTITUDE);
+			BackGround b = new BackGround(random, 10, MAX_STAR_AGE + rand.nextInt(MAX_STAR_AGE));
 			// Apply a vertical force with some random horizontal component
 			ssLandry.force.x = rand.nextFloat() * 0.00095f * (rand.nextBoolean() == true ? -1 : 1);
 			ssLandry.force.y = rand.nextFloat() * 0.00095f * (rand.nextBoolean() == true ? -1 : 1);
 			b.setBodyActive(false);
-			// TRY DE FAIRE BOUGER LES ETOILES VERS LA GAUCHE EN MODE LE FOND BOUGE MAIS PAS REUSSI A CONTRER LA GRAVITE !
-			//b.applyBodyForceToCenter(100, 0, true);
-			//b.applyBodyLinearImpulse(new Vector2(100f, 100f), random, true);
-			
-			
+			// TRY DE FAIRE BOUGER LES ETOILES VERS LA GAUCHE EN MODE LE FOND BOUGE MAIS PAS
+			// REUSSI A CONTRER LA GRAVITE !
+			// b.applyBodyForceToCenter(100, 0, true);
+			// b.applyBodyLinearImpulse(new Vector2(100f, 100f), random, true);
+
 		}
 	}
-	
+
 	public void onDrag(int x, int y) {
 		super.onDrag(x, y);
 		positionClick.x = x;
 		positionClick.y = y;
 	}
+
 	@Override
 	public void onClick(int x, int y, int button) {
 		super.onClick(x, y, button);
 		mouseActive = true;
 		positionClick = new Vector2(x, y);
-		System.out.println("X"+x);
-		System.out.println("Y"+y);
+//		System.out.println("X" + x);
+//		System.out.println("Y" + y);
 	}
-	
+
 	@Override
 	public void onRelease(int x, int y, int button) {
 		super.onRelease(x, y, button);
@@ -159,43 +163,42 @@ public class LunarLander_Main extends PortableApplication{
 		positionClick.y = y;
 		mouseActive = false;
 	}
-	
+
 	@Override
 	public void onKeyUp(int keycode) {
 		switch (keycode) {
-			case Input.Keys.UP:
-				ssLandry.thrustUp = false;
-				break;
-			case Input.Keys.LEFT:
-				ssLandry.thrustLeft = false;
-				break;
-			case Input.Keys.RIGHT:
-				ssLandry.thrustRight = false;
-				break;
-			default:
-				break;
+		case Input.Keys.UP:
+			ssLandry.thrustUp = false;
+			break;
+		case Input.Keys.LEFT:
+			ssLandry.thrustLeft = false;
+			break;
+		case Input.Keys.RIGHT:
+			ssLandry.thrustRight = false;
+			break;
+		default:
+			break;
 		}
 	}
 
 	@Override
 	public void onKeyDown(int keycode) {
 		switch (keycode) {
-			case Input.Keys.UP:
-				ssLandry.thrustUp = true;
-				break;
-			case Input.Keys.LEFT:
-				ssLandry.thrustLeft = true;
-				break;
-			case Input.Keys.RIGHT:
-				ssLandry.thrustRight = true;
-				break;
-			default:
-				break;
+		case Input.Keys.UP:
+			ssLandry.thrustUp = true;
+			break;
+		case Input.Keys.LEFT:
+			ssLandry.thrustLeft = true;
+			break;
+		case Input.Keys.RIGHT:
+			ssLandry.thrustRight = true;
+			break;
+		default:
+			break;
 		}
 	}
 
 	public static void main(String[] args) {
-
 		new LunarLander_Main();
 	}
 }
